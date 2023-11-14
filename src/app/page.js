cdb2ede883ca1e41cc2c01b97fc453ed104dd80b
Pratik Soni <pratiksoni1402@gmail.com>
2023-11-14 16:38:53 +0530

    next basics

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
+
 import Image from 'next/image'
+
 
 export default function Home() {
     const trail= () =>{
@@ -6,8 +8,8 @@ export default function Home() {
     }
     return (
         <main className="">
-            <h1>Hello </h1>
-                <button onClick={()=>{trail}}>Click Me</button>
+            <h1>Hello</h1>
+                <button>Click Me</button>
         </main>
     )
 }
