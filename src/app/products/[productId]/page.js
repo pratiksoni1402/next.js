bfafb8fa757ff2da85c9dab014527660e3a379eb
Pratik Soni <pratiksoni1402@gmail.com>
2023-11-16 16:04:18 +0530

    dynamic routing

---
+++ src/app/products/[productId]/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProductDetails ({params}) {
+  return (
+    <div>ProductDetails {params.productId}</div>
+  )
+}
---
+++ src/app/products/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ProductList() {
+    return (
+        <>
+            <div>Product List</div>
+            <h2>Product 1</h2>
+            <h2>Product 2</h2>
+            <h2>Product 3</h2>
+            <h2>Product 4</h2>
+            <h2>Product 5</h2>
+        </>
+    )
+}
