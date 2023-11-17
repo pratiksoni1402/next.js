3ed8f48099e2c3aab53da2113bb27dce673d5751
Pratik Soni <pratiksoni1402@gmail.com>
2023-11-17 13:48:46 +0530

    resetting project

--- src/app/blog/first/page.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function FirstBlogPost() {
-  return (
-    <div>FirstBlogPost</div>
-  )
-}
--- src/app/products/[productId]/reviews/[reviewId]/page.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function ReviewDetail({params}) {
-  return (
-    <div>Review{params.reviewId} for product {params.productId}</div>
-    
-  )
-}
