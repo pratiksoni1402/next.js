94ec86a54841383704ddcb0b214e0cc726de1307
Pratik Soni <pratiksoni1402@gmail.com>
2023-11-17 18:22:14 +0530

    implementing common layout for login page

---
+++ src/app/login/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function layout() {
+    return (
+        <>
+            <div>
+                <h1>This is Common page layout for teacher and student login</h1>
+                
+            </div>
+        </>
+    )
+}
