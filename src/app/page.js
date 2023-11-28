7eff0d39f415377e0661a3a22f7556ac2b7afc60
Pratik Soni <pratiksoni1402@gmail.com>
2023-11-28 15:12:13 +0530

    removing unnecessary comments

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
-// 'use client'
+
 import Image from 'next/image'
-// import Link from 'next/link'
-// import { useRouter } from 'next/navigation'
 import Profile from '../../public/uploads/images/kitchen.jpg'
 export default function Home() {
     console.log(process.env.DB_PASSWORD)
