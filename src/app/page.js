54f462e5d79ad11a36a4d21e0fe5b7d435620e80
Pratik <pratiksoni1402@gmail.com>
2023-12-06 06:56:31 +0000

    putting all images in public folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 
 import Image from 'next/image'
-import Profile from '../../public/uploads/images/kitchen.jpg'
+import Profile from '../../public/uploads/images/homepage/kitchen.jpg'
 export default function Home() {
-    console.log(process.env.DB_PASSWORD)
+console.log(process.env.DB_PASSWORD)
 
-    
+
     return (
         <main className="">
             <Image src={Profile} alt='' />
+            <p>Creating API</p>
+            
             <p>Creating API</p>
 
         </main>
